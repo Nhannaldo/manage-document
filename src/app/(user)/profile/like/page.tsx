1b4f66e864c41f70e24107fdf42bbd2a90168d64
Nhannaldo <nguyenhuunhan3354@gmail.com>
2024-10-22 10:06:15 +0700

    feat: integration api render favorite document

--- src/app/(user)/profile/like/page.tsx
+++ src/app/(user)/profile/like/page.tsx
@@ -1,11 +1,50 @@
 'use client';
-import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
+import DocumentItem from '@/components/DocumentItem';
+import { useUser } from '@/context/UserContext';
 
+interface IDocumentItem {
+    _id: string;
+    title: string;
+    description?: string;
+    categoryId: string;
+    subjectId: string;
+    fileUrl: string;
+    imageUrl: string;
+    typefileId: string;
+    pagenumber: number;
+    views: number;
+    downloads: number;
+    uploadedBy: string;
+    status: boolean;
+    sharedBy?: string[];
+    uploadedAt?: Date;
+    approvedAt?: Date;
+}
 export default function Like() {
-    const [value, setValue] = React.useState('one');
+    const [favoriteDocuments, setFavoriteDocuments] = useState<IDocumentItem[]>(
+        [],
+    );
+    const { user } = useUser();
+
+    useEffect(() => {
+        const fetchFavoriteDocuments = async () => {
+            const response = await fetch(
+                `http://localhost:3001/favorite/get-all-document-favorite/${user._id}`,
+            );
+            if (!response.ok) {
+                throw new Error('Failed to fetch favorite documents');
+            }
+            const data = await response.json();
+
+            // Truy cập vào documentId trong mỗi phần tử
+            const documents = data.map((item: any) => item.documentId);
+            setFavoriteDocuments(documents);
+        };
+        fetchFavoriteDocuments();
+    }, [user._id]);
+    console.log(favoriteDocuments);
 
     return (
         <div className="">
@@ -23,7 +62,20 @@ export default function Like() {
                     />
                 </div>
             </div>
-            <div className="mt-3">Hiện chưa có tài liệu yêu thích</div>
+            {/* <div className="mt-3">Hiện chưa có tài liệu yêu thích</div> */}
+            <div>
+                {/* Document List */}
+                <ul className="grid grid-cols-4 gap-3 mt-4">
+                    {favoriteDocuments.map((item, index) => (
+                        <li
+                            className="bg-[#fff] border border-[#ececec] hover:translate-y-[-4px]"
+                            key={item._id}
+                        >
+                            <DocumentItem props={item} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
